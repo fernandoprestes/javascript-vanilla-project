0d7167666a321c94ce1fa1ac310f97e4354fc423
Fernando Prestes <fernandoprestesc@gmail.com>
2021-08-23 15:15:26 -0300

    🎨 format code app counter

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,29 +1,17 @@
-let count = 0
-
 const value = document.querySelector('.value')
 const btns = document.querySelectorAll('.btn')
+
+let count = 0
 
 btns.forEach(function (btn) {
   btn.addEventListener('click', event => {
     const styles = event.currentTarget.classList
-
-    if (styles.contains('decrease')) {
-      count--
-    } else if (styles.contains('increase')) {
-      count++
-    } else {
-      count = 0
-    }
-
-    if (count > 0) {
-      value.style.color = '#22aa22'
-    }
-    if (count < 0) {
-      value.style.color = '#ee2222'
-    }
-    if (count === 0) {
-      value.style.color = '#222'
-    }
+    if (styles.contains('increase')) count++
+    if (styles.contains('reset')) count = 0
+    if (styles.contains('decrease')) count--
+    if (count > 0) value.style.color = '#22aa22'
+    if (count === 0) value.style.color = '#222'
+    if (count < 0) value.style.color = '#ee2222'
     value.textContent = count
   })
 })
